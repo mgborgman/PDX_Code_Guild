279f6e6b76f023c576904ea07e0119da7c0ec969
Matt Borgman <mgborgman@gmail.com>
2015-02-26 13:18:26 -0800

    cmd line ecommerce project

---
+++ online_store/store.py
@@ -0,0 +1,59 @@
+__author__ = 'Matt Borgman'
+
+
+class Store(object):
+    def __init__(self):
+        self.dict_of_customers = {}
+        self.available_items = []
+
+
+class Customer(object):
+    def __init__(self, name='none', email='none'):
+        self.name = name
+        self.email = email
+        self.cart = Cart()
+
+
+class Cart(object):
+    def __init__(self):
+        pass
+
+
+class Menu(object):
+    def __init__(self):
+        pass
+
+
+    def print_menu_main(self):
+        print("BetterBuy.com")
+        print("1. Log in")
+        print("2. Sign up")
+        print("3. Exit")
+        menu_select = raw_input("Please Log in to shop with us! \n")
+        return menu_select
+
+
+    def use_menu(self, select, store):
+        try:
+            if select == '1':
+                pass
+            elif select == '2':
+                self.sign_up(store)
+        except ValueError:
+            print("Please enter a number")
+
+
+    def sign_up(self, store):
+        name = raw_input("What is your name? ")
+        email = raw_input("What is your email? ")
+        store.dict_of_customers[name] = email
+
+
+better_buy = Store()
+main = Menu()
+select = main.print_menu_main()
+main.use_menu(select, better_buy)
+print(better_buy.dict_of_customers)
+select = main.print_menu_main()
+main.use_menu(select, better_buy)
+print(better_buy.dict_of_customers)
